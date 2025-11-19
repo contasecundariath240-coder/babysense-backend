783cd2c7d435e676c24733cb61ccfe4fdf6cf474
Thiago Martinny <contasecundariath240@gmail.com>
2025-11-19 11:56:40 -0300

    Update measurements.py

--- routers/measurements.py
+++ routers/measurements.py
@@ -1,20 +1,24 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from database import get_db
-import models, schemas
+from src.database import get_db
+from src import models
+import schemas
 
 router = APIRouter()
 
+
 @router.post("/", response_model=schemas.MeasurementResponse)
-def create_measurement(
-    measurement: schemas.MeasurementCreate,
-    db: Session = Depends(get_db)
-):
-    new_measurement = models.Measurement(**measurement.dict())
-    db.add(new_measurement)
+def create_measurement(data: schemas.MeasurementCreate, db: Session = Depends(get_db)):
+    baby = db.query(models.Baby).filter(models.Baby.id == data.baby_id).first()
+    if not baby:
+        raise HTTPException(status_code=404, detail="Baby not found")
+
+    new_m = models.Measurement(**data.dict())
+    db.add(new_m)
     db.commit()
-    db.refresh(new_measurement)
-    return new_measurement
+    db.refresh(new_m)
+    return new_m
+
 
 @router.get("/", response_model=list[schemas.MeasurementResponse])
 def list_measurements(db: Session = Depends(get_db)):
