41dd5d99d8a3c3b0540eb940b4291c9315c7d230
Thiago Martinny <contasecundariath240@gmail.com>
2025-11-19 08:36:50 -0300

    Delete src/routes directory

--- src/routes/app.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routes.measurements import measurements_bp
-from routes.babies import babies_bp
-
-app = FastAPI()
-
-app.include_router(measurements_bp)
-app.include_router(babies_bp)
