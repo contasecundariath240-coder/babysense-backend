daeefd706febd3d3c907465716d1d283655884fb
Thiago Martinny <contasecundariath240@gmail.com>
2025-11-19 11:53:03 -0300

    Update _init_.py

--- routers/_init_.py
+++ routers/_init_.py
@@ -1,8 +1,1 @@
-from fastapi import APIRouter
-from .babies import router as babies_router
-from .measurements import router as measurements_router
 
-router = APIRouter()
-
-router.include_router(babies_router, prefix="/babies", tags=["Babies"])
-router.include_router(measurements_router, prefix="/measurements", tags=["Measurements"]) 
