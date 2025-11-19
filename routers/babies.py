878eb707cae68c0a606ecf630eb0a30a2fb5cf99
Thiago Martinny <contasecundariath240@gmail.com>
2025-11-19 08:43:18 -0300

    Create babies.py

---
+++ routers/babies.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import get_db
+import models, schemas
+
+router = APIRouter()
+
+@router.post("/", response_model=schemas.BabyResponse)
+def create_baby(baby: schemas.BabyCreate, db: Session = Depends(get_db)):
+    new_baby = models.Baby(**baby.dict())
+    db.add(new_baby)
+    db.commit()
+    db.refresh(new_baby)
+    return new_baby
+
+@router.get("/", response_model=list[schemas.BabyResponse])
+def list_babies(db: Session = Depends(get_db)):
+    return db.query(models.Baby).all() 
