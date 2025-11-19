453f0801d809f5f3f7de04ca890f01ae1e380540
Thiago Martinny <contasecundariath240@gmail.com>
2025-11-19 00:41:20 -0300

    Update app.py

--- src/routes/app.py
+++ src/routes/app.py
@@ -1,2 +1,8 @@
-from routes.measurements import measurements_bp 
+from fastapi import FastAPI
+from routes.measurements import measurements_bp
 from routes.babies import babies_bp
+
+app = FastAPI()
+
+app.include_router(measurements_bp)
+app.include_router(babies_bp)
