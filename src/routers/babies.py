53d40a9da8f4f4df68a7cc9260ca805487e2ba37
Thiago Martinny <contasecundariath240@gmail.com>
2025-11-19 13:06:38 -0300

    Create babies.py

---
+++ src/routers/babies.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from src.routers import babies, measurements
+from src.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(babies.router, prefix="/babies", tags=["Babies"])
+app.include_router(measurements.router, prefix="/measurements", tags=["Measurements"])
+
+@app.get("/")
+def root():
+    return {"message": "Backend is running!"} 
